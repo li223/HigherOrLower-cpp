581f8b12ad90e7713961f1935003c71d8f768c3f
li223 <li223@users.noreply.github.com>
2020-04-26 14:07:51 +0100

    Add project files.

---
+++ HigherOrLower-cpp/HigherOrLower-cpp.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <ctime>
+#include "ToLower.h"
+
+int main()
+{
+    srand(time(NULL));
+    int score = 0;
+    while (true)
+    {
+        auto rannum = std::rand() % 100;
+        auto nextnum = std::rand() % 100;
+        std::cout << "Is the next number is higher or lower than " << rannum << ": ";
+        std::string guess;
+        std::cin >> guess;
+        std::cout << std::endl;
+        ToLower(guess);
+        while (guess != "higher" && guess != "lower")
+        {
+            std::cout << "\nPlease enter 'higher' or 'lower': ";
+            std::cin >> guess;
+            ToLower(guess);
+        }
+        while (true)
+        {
+            if (guess == "higher" && rannum < nextnum)
+            {
+                score++;
+                std::cout << "Correct" << std::endl;
+                break;
+            }
+            else if (guess == "lower" && rannum > nextnum)
+            {
+                score++;
+                std::cout << "Correct" << std::endl;
+                break;
+            }
+            else
+            {
+                std::cout << "Incorrect" << std::endl;
+                score--;
+                break;
+            }
+        }
+        std::cout << "Score: " << score << "\n\n";
+    }
+    return 0;
+}
---
+++ HigherOrLower-cpp/ToLower.h
@@ -0,0 +1,9 @@
+#include <string>
+
+void ToLower(std::string& str)
+{
+    for (unsigned int i = 0; i < str.length(); i++)
+    {
+        str[i] = tolower(str[i]);
+    }
+}
